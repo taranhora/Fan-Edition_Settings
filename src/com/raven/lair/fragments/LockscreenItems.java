7ec212cba92c172b2b7394923ddf64918d191042
beanstown106 <nbenis106@gmail.com>
2017-09-23 16:33:19 +0800

    den: Fingerprint authentication vibration [2/2]
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: niteshkumar2000 <nitesh156200@gmail.com>

--- src/com/raven/lair/fragments/LockscreenItems.java
+++ src/com/raven/lair/fragments/LockscreenItems.java
@@ -39,15 +39,36 @@ import java.util.List;
 public class LockscreenItems extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+    private FingerprintManager mFingerprintManager;
+    private SwitchPreference mFingerprintVib;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lockscreen_items);
+    	 mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+        mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
+        if (!mFingerprintManager.isHardwareDetected()){
+            prefScreen.removePreference(mFingerprintVib);
+        } else {
+            mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+            mFingerprintVib.setOnPreferenceChangeListener(this);
+        }
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+	ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mFingerprintVib) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FINGERPRINT_SUCCESS_VIB, value ? 1 : 0);
+            return true;
+        }
+    	return false;
     }
 
     @Override
